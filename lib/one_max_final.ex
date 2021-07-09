91c520e0608fe237d88f401eaf122d08c85a803c
raymondboswel <raymondboswel@gmail.com>
2021-07-09 06:30:44 +0200

    Implemented improved genetic algorithm framework

---
+++ lib/one_max_final.ex
@@ -0,0 +1,19 @@
+defmodule OneMax do
+  @behaviour Problem
+  alias Types.Chromosome
+  @impl true
+  def genotype do
+    genes = for _ <- 1..42, do: Enum.random(0..1)
+    %Chromosome{genes: genes, size: 42}
+  end
+
+  @impl true
+  def fitness_function(chromosome), do: Enum.sum(chromosome.genes)
+
+  @impl true
+  def terminate?([best | _]), do: best.fitness == 42
+
+  def solve do
+    Genetic.run(__MODULE__)
+  end
+end
